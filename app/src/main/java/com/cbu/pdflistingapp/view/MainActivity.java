d1121ae63f85c0dea9e56261af66eeec6946d36f
irfan <irfanysoydan@gmail.com>
2022-05-12 22:37:09 +0300

    Initial commit

---
+++ app/src/main/java/com/cbu/pdflistingapp/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cbu.pdflistingapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cbu.pdflistingapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
