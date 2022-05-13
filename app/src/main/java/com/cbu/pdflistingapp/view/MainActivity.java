57e7b188f7430a066a3db92c95a848f9fddf385b
irfan <irfanysoydan@gmail.com>
2022-05-13 23:18:01 +0300

    Retrofit modeli ve interface oluşturuldu.

--- app/src/main/java/com/cbu/pdflistingapp/view/MainActivity.java
+++ app/src/main/java/com/cbu/pdflistingapp/view/MainActivity.java
@@ -5,12 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.cbu.pdflistingapp.R;
+import com.cbu.pdflistingapp.model.IPDFDataService;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Retrofit retrofit;
+    private IPDFDataService dataService;
+    private String baseUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        retrofit  = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        dataService = retrofit.create(IPDFDataService.class);
     }
 }
