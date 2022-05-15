215e935806505e7e0caa828332bb3c3d7a07676c
irfan <irfanysoydan@gmail.com>
2022-05-15 19:22:22 +0300

    Retrofit Yapısı Düzenlendi

--- app/src/main/java/com/cbu/pdflistingapp/view/MainActivity.java
+++ app/src/main/java/com/cbu/pdflistingapp/view/MainActivity.java
@@ -1,34 +1,18 @@
 package com.cbu.pdflistingapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.cbu.pdflistingapp.R;
 import com.cbu.pdflistingapp.model.IPDFDataService;
 import com.cbu.pdflistingapp.model.RetrofitInstance;
-import com.cbu.pdflistingapp.model.RetrofitModel;
-import com.cbu.pdflistingapp.model.ServiceGenerator;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.List;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,81 +35,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
-        /*private boolean writeResponseBodyToDisk(ResponseBody body) {
-            try {
-                // todo change the file location/name according to your needs
-                File futureStudioIconFile = new File(getExternalFilesDir(null) + File.separator + "Future Studio Icon.png");
-
-                InputStream inputStream = null;
-                OutputStream outputStream = null;
-
-                try {
-                    byte[] fileReader = new byte[4096];
-
-                    long fileSize = body.contentLength();
-                    long fileSizeDownloaded = 0;
-
-                    inputStream = body.byteStream();
-                    outputStream = new FileOutputStream(futureStudioIconFile);
-
-                    while (true) {
-                        int read = inputStream.read(fileReader);
-
-                        if (read == -1) {
-                            break;
-                        }
-
-                        outputStream.write(fileReader, 0, read);
-
-                        fileSizeDownloaded += read;
-
-                        Log.e("File Download: " , fileSizeDownloaded + " of " + fileSize);
-                    }
-
-                    outputStream.flush();
-
-                    return true;
-                } catch (IOException e) {
-                    return false;
-                } finally {
-                    if (inputStream != null) {
-                        inputStream.close();
-                    }
-
-                    if (outputStream != null) {
-                        outputStream.close();
-                    }
-                }
-            } catch (IOException e) {
-                return false;
-            }
-        }
-
-
-            IPDFDataService apiInterface = ServiceGenerator.createService(IPDFDataService.class);
-
-            Call<ResponseBody> call = IPDFDataService.downloadPDF();
-
-            call.enqueue(new Callback<ResponseBody>() {
-                @Override
-                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                    if (response.isSuccessful()){
-                        boolean writtenToDisk = writeResponseBodyToDisk(response.body());
-
-                        Log.e("success","File download was a success? ", String.valueOf(writtenToDisk));
-                    }
-                }
-
-                @Override
-                public void onFailure(Call<ResponseBody> call, Throwable t) {
-
-                }
-            });
-
-
-
-*/
 
     }
 }
