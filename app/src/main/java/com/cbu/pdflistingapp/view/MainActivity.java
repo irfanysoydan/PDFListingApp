d65e3d1ef52fb83aeb920dcdab809634ed96af86
rasenshuriken <omerozoglu.1@gmail.com>
2022-05-19 17:35:20 +0300

    Düzeltmeler yapıldı ve delete için AlertDialog çıkartıldı

--- app/src/main/java/com/cbu/pdflistingapp/view/MainActivity.java
+++ app/src/main/java/com/cbu/pdflistingapp/view/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cbu.pdflistingapp.view;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -7,13 +8,11 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.PopupMenu;
@@ -24,14 +23,14 @@ import com.cbu.pdflistingapp.databinding.ActivityMainBinding;
 import com.cbu.pdflistingapp.model.PDFModel;
 import com.cbu.pdflistingapp.view.adapter.ExpandableListViewAdapter;
 import com.cbu.pdflistingapp.viewmodel.MainViewModel;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import okhttp3.ResponseBody;
-
 public class MainActivity extends AppCompatActivity {
+    private String TAG = this.getClass().getName();
     private static final int MY_PERMISSIONS_REQUEST = 100;
     private ActivityMainBinding viewBinding;
     private MainViewModel mainViewModel;
@@ -40,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
     private ExpandableListView expandableListView;
     private ExpandableListAdapter expandableListAdapter;
     private List<String> expandableTitleList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,22 +47,26 @@ public class MainActivity extends AppCompatActivity {
         View view = viewBinding.getRoot();
         setContentView(view);
 
-        if(ContextCompat.checkSelfPermission(MainActivity.this,
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+        checkSelfPermission();
+        fillExpandableListView();
+        floatingButtonListeners();
+
+
+    }
+
+    private void checkSelfPermission(){
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this,
                     new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                     MY_PERMISSIONS_REQUEST);
         }
-
-        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
-       fillExpandableListView();
-
-
-
     }
 
-   private void setExpandableListView(){
+    private void setExpandableListView() {
         expandableListView = viewBinding.mainExpandableListView;
         expandableTitleList = new ArrayList<>(pdfModelList.keySet());
         expandableListAdapter = new ExpandableListViewAdapter(this, expandableTitleList, pdfModelList) {
@@ -70,51 +74,74 @@ public class MainActivity extends AppCompatActivity {
         expandableListView.setAdapter(expandableListAdapter);
     }
 
-   private void expandableListViewListeners(){
-       expandableListView.setOnItemLongClickListener((parent, view, position, id) -> {
-           PDFModel model = (PDFModel) parent.getItemAtPosition(position);
-           if (ExpandableListView.getPackedPositionType(id) == ExpandableListView.PACKED_POSITION_TYPE_CHILD) {
-               PopupMenu popup = new PopupMenu(MainActivity.this, view);
-               MenuInflater inflater = popup.getMenuInflater();
-               inflater.inflate(R.menu.detail_menu, popup.getMenu());
-               popup.setOnMenuItemClickListener(menuItem -> {
-                   switch (menuItem.getItemId()) {
-                       case R.id.menu_download:
-                           downloadPDF(model.getId());
-                           return true;
-                       case R.id.menu_delete:
-                           deletePDF(model.getId());
-                           return true;
-                       case R.id.menu_detail:
-                           return true;
-                       default:
-                           return false;
-                   }
-               });
-               popup.show();
-               return true;
-           }
-           return false;
-       });
-   }
+    private void expandableListViewListeners() {
+        expandableListView.setOnItemLongClickListener((parent, view, position, id) -> {
+            PDFModel model = (PDFModel) parent.getItemAtPosition(position);
+            if (ExpandableListView.getPackedPositionType(id) == ExpandableListView.PACKED_POSITION_TYPE_CHILD) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, view);
+                MenuInflater inflater = popup.getMenuInflater();
+                inflater.inflate(R.menu.detail_menu, popup.getMenu());
+                popup.setOnMenuItemClickListener(menuItem -> {
+                    switch (menuItem.getItemId()) {
+                        case R.id.menu_download:
+                            downloadPDF(model.getId());
+                            return true;
+                        case R.id.menu_delete:
+                            deletePDF(model.getId());
+                            return true;
+                        case R.id.menu_detail:
+                            return true;
+                        default:
+                            return false;
+                    }
+                });
+                popup.show();
+                return true;
+            }
+            return false;
+        });
+    }
 
-   private void fillExpandableListView(){
+    private void fillExpandableListView() {
         mainViewModel.getAllGroupedPDFs().observe(this, pdfModels -> {
             pdfModelList = pdfModels;
             setExpandableListView();
             expandableListViewListeners();
         });
-   }
+    }
 
-   private void downloadPDF(String id){
+    private void downloadPDF(String id) {
         mainViewModel.downloadPDF(id).observe(this, responseBody -> {
         });
-   }
+    }
 
-   private void deletePDF(String id){
-        mainViewModel.deletePDF(id).observe(this, model -> {
-            Toast.makeText(MainActivity.this,model.getName()+" Was deleted",Toast.LENGTH_LONG).show();
-            fillExpandableListView();
+    private void deletePDF(String id) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setMessage("Are you sure you really want to delete the this PDF file?");
+        alertBuilder.setCancelable(true);
+        alertBuilder.setPositiveButton("Yes, I'm sure.",(dialog, i) -> {
+            mainViewModel.deletePDF(id).observe(this, model -> {
+                Toast.makeText(MainActivity.this, model.getName() + " Was deleted", Toast.LENGTH_LONG).show();
+                fillExpandableListView();
+            });
+            dialog.cancel();
         });
-   }
+        alertBuilder.setNegativeButton("No",(dialog,i) -> dialog.cancel());
+        AlertDialog alertDialog = alertBuilder.create();
+        alertDialog.show();
+    }
+
+    private void addPDF() {
+        mainViewModel.createPDF(null).observe(this, model -> {
+
+        });
+    }
+
+    private void floatingButtonListeners() {
+        viewBinding.fab.setOnClickListener(view -> {
+            // addPDF();
+            Toast.makeText(MainActivity.this, "TEST", Toast.LENGTH_LONG).show();
+        });
+    }
+
 }
